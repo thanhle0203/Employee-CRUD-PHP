c558ce3c9b312da7896a275e321acb20f30cd5c2
Thanh Le <thanhlevan0203@gmail.com>
2022-08-25 01:16:19 -0700

    If delete button clicked, delete record from data using passed ud

--- process.php
+++ process.php
@@ -9,3 +9,8 @@ if (isset($_POST['save'])) {
     $mysqli->query("INSERT INTO data (name, location) VALUES ('$name', '$location')") or
     die($mysqli->error);
 }
+
+if (isset($_GET['delete'])) {
+    $id = $_GET['delete'];
+    $mysqli->query("DELETE FROM data WHERE id=$id") or die($mysqli->error());
+}
