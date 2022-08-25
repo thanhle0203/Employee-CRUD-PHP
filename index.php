eb2fd01108fc31e2143345e2fb92420e51fb913e
Thanh Le <thanhlevan0203@gmail.com>
2022-08-25 01:09:17 -0700

    Add Edit & Delete link buttons, pass id of record as GET variable in URL in both links

--- index.php
+++ index.php
@@ -35,7 +35,12 @@
                 <tr>
                     <td><?php echo $row['name']; ?></td>
                     <td><?php echo $row['location']; ?></td>
-                    <td></td>
+                    <td>
+                        <a href="index.php?edit=<?php echo $row['id']; ?>"
+                        class="btn btn-info">Edit</a>
+                        <a href="process.php?delete=<?php echo $row['id']; ?>"
+                        class="btn btn-danger">Delete</a>
+                    </td>
                 </tr>
             <?php endwhile; ?>
             </table>
